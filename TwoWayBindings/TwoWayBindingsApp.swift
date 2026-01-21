ae2deaa88aa6b591955d0eff4613fd5cb2a08b61
Theadora Yocum <theayocum@mac.com>
2026-01-21 14:27:18 -0500

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Thea Yocum on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
